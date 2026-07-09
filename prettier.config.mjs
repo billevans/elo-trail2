47dc68c8326659d271aa42f708b22b98f8e52f76
billevans <bill.sunshine@gmail.com>
2026-07-09 11:34:26 +0000

    chore: add development tooling

---
+++ prettier.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Config} */
+const config = {
+  plugins: ["prettier-plugin-tailwindcss"],
+
+  semi: true,
+
+  singleQuote: false,
+
+  trailingComma: "all",
+
+  printWidth: 80,
+
+  tabWidth: 2,
+
+  useTabs: false,
+};
+
+export default config;
